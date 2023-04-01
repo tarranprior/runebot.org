7d8ff0e95ef338282c931f1c6e69aaf9c3a7d8b2
Tarran Prior <Tarranprior@protonmail.com>
2023-04-02 00:37:23 +0100

    Update /runebot.gg/: add initial variables and `GlobalStyles`.

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+import variables from "../styles/variables";
+
+const GlobalStyle = createGlobalStyle`
+  ${variables};
+`;
+
+export default GlobalStyle;
---
+++ src/styles/variables.js
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+
+const variables = css`
+  :root {
+    --dark-grey: #1D1E28;
+    --grey: #2B2C3B;
+    --light-grey: #C4C4CB;
+    --lightest-grey: #E7E7EA;
+    --white: #F3F3F6;
+    --blurple: #5865F2;
+  }
+`;
+
+export default variables;
