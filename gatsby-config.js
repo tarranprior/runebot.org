c5dfc7ab83852f8cc445737b6f0d8035a7f0394e
Tarran Prior <Tarranprior@protonmail.com>
2023-04-02 00:03:16 +0100

    💅 Update /runebot.gg/: install `styled-components`.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,5 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-styled-components`
+  ],
 }
