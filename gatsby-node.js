52cb76e896251a99d15802c428f041b13031aaf1
Tarran Prior <Tarranprior@protonmail.com>
2023-10-16 20:07:25 +0100

    Update /runebot.org/

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,9 @@ exports.createPages = async ({ actions }) => {
   const { createRedirect } = actions
   
   createRedirect({
-    fromPath: `/invite/*`,
+    fromPath: `/invite`,
     toPath: `https://discord.com/oauth2/authorize?client_id=978953033989914654&permissions=2147764224&scope=bot%20applications.commands`,
-    statusCode: 200,
+    redirectInBrowser: true,
+    isPermanent: true
   })
 }
